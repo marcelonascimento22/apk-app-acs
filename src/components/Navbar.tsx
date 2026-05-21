b394cd6c4cfdead654f9078f3a1402916ae473ae
Marcelo Nascimento <marcelo.augustonasci@gmail.com>
2026-05-21 20:03:23 -0300

    Correção do controlhe de usuarios para funções do  menu de navegação

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,8 +11,8 @@ const Navbar = () => {
   const [search, setSearch] = useState("");
   const [searchDebounced, setSearchDebounced] = useState("");
   const usuario = JSON.parse(localStorage.getItem('usuario') || '{}');
-  console.log("USUÁRIO NO NAVBAR:", usuario);
-  const isAdmin = usuario?.perfil === 'admin';
+  //console.log("USUÁRIO NO NAVBAR:", usuario);
+  const isAdmin = usuario?.perfil != 'ACS';
 
   // debounce
   useEffect(() => {
